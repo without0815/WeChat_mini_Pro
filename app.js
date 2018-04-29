236ff620619634014a513699ca78fb41774657cd
yingmm <without_0815@163.com>
2018-04-30 00:41:01 +0800

    init app

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 //app.js
 //var util = require('utils/util.js');
-App({
 
-  onLaunch: function () { 
-    
+App({
+  onLaunch: function () {
+    console.warn('onLaunch  is runing');
+  },
+  onShow: function () {
+    console.warn('onShow  is runing');
+  },
+  onHide: function () {
+    console.warn('onHide  is runing');
+  },
+  onError: function (msg) {
+    console.warn('onError  is runing');
   }
-  
 })
